b5dffa8829aaf962f9f87e672cdbfa5e8febdec8
liuzeyu <liuzeyu246@gmail.com>
2023-09-26 13:00:18 -0500

    first

---
+++ src/main/java/com/example/korera/KoreraApplication.java
@@ -0,0 +1,13 @@
+package com.example.korera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoreraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KoreraApplication.class, args);
+    }
+
+}
